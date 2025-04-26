ba682903df5ce18bcddb745ce3b49973fd2ff82f
BrandMineGamer079 <165478025+BrandMineGamer079@users.noreply.github.com>
2025-04-26 18:53:40 +0530

    Update ticketicons.js

--- UI/icons/ticketicons.js
+++ UI/icons/ticketicons.js
@@ -5,6 +5,7 @@ const ticketIcons = {
     heartIcon: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=66a571e8&is=66a42068&hm=0761d49758b73e8bbe8785e7998acfd2f1f79b4f623f024072468d4420cc102e&',
     modIcon :'https://cdn.discordapp.com/emojis/805980519644004372.gif',
     pingIcon : 'https://cdn.discordapp.com/emojis/981302000694210620.gif',
+    emoji : 'https://cdn.discordapp.com/attachments/1365563106713141298/1365679218364649502/none.gif?ex=680e2f62&is=680cdde2&hm=8cff3d41e2ada3c358018705644b64d8e431f315591f94239780cec909d3259d&',
 };
 
 module.exports = ticketIcons;
