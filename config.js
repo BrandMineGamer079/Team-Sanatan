187a0c432ebe6eabd3bb5a4c8bce870a2e0e1942
BrandMineGamer079 <165478025+BrandMineGamer079@users.noreply.github.com>
2025-04-26 13:06:55 +0530

    Update config.js

--- config.js
+++ config.js
@@ -3,15 +3,14 @@ const { ActivityType } = require('discord.js');
 module.exports = {
   ownerId: '1052983625425100812,1026512578958077972',
   status: {
-    rotateDefault: async (client) => {
-      const guild = client.guilds.cache.first(); // Assuming the bot is in only one guild
-      const memberCount = guild ? guild.memberCount : 0;
-      return [
-        { name: `Watching ${memberCount} members`, type: ActivityType.Watching },
-      ];
-    },
-    songStatus: true,
+    rotateDefault: [
+      { name: `Watching ${memberCount} members`, type: ActivityType.Watching },
+      { name: `Watching ${memberCount} members`, type: ActivityType.Watching },
+      { name: `Watching ${memberCount} members`, type: ActivityType.Watching },
+      { name: `Watching ${memberCount} members`, type: ActivityType.Watching },
+    ],
+    songStatus: true
   },
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
-};
+}
