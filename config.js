440abcb67d1c5695a04383e91560171c55a043f1
BrandMineGamer079 <165478025+BrandMineGamer079@users.noreply.github.com>
2025-04-26 10:42:08 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,16 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
-  status: {
-    rotateDefault: [
-      { name: 'Netflix', type: ActivityType.Watching },
-      { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
-      { name: 'Spotify', type: ActivityType.Listening },
-    ],
-    songStatus: true
-  },
+  ownerId: '1026512578958077972,1052983625425100812',
+  
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
-}
+  
+  updateWatchingActivity(client) {
+    const guild = client.guilds.cache.get('1297483796756824186'); // Replace with your server ID
+    if (guild) {
+      const memberCount = guild.memberCount;
+      client.user.setActivity(`Watching over ${memberCount} members`, { type: ActivityType.Watching });
+    }
+  }
+};
