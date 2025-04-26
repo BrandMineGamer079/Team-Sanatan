72bf03f3ca45bfe1f92f3f94abd4349f3b466cdb
BrandMineGamer079 <165478025+BrandMineGamer079@users.noreply.github.com>
2025-04-26 12:43:24 +0530

    Update config.js

--- config.js
+++ config.js
@@ -15,12 +15,3 @@ module.exports = {
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
 };
-
-// Updating the bot status
-client.on('ready', async () => {
-  console.log(`${client.user.tag} is now online!`);
-
-  const statuses = await module.exports.status.rotateDefault(client);
-  const status = statuses[0]; // Since there's only one status now
-  client.user.setActivity(status.name, { type: status.type });
-});
