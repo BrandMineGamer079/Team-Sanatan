9d89c82a8d9b653196141f2eae0b76938366121f
BrandMineGamer079 <165478025+BrandMineGamer079@users.noreply.github.com>
2025-04-26 12:38:12 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,16 +1,26 @@
 const { ActivityType } = require('discord.js');
 
 module.exports = {
-  ownerId: '1004206704994566164',
+  ownerId: '1052983625425100812,1026512578958077972',
   status: {
-    rotateDefault: [
-      { name: 'Netflix', type: ActivityType.Watching },
-      { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
-      { name: 'Spotify', type: ActivityType.Listening },
-    ],
-    songStatus: true
+    rotateDefault: async (client) => {
+      const guild = client.guilds.cache.first(); // Assuming the bot is in only one guild
+      const memberCount = guild ? guild.memberCount : 0;
+      return [
+        { name: `Watching ${memberCount} members`, type: ActivityType.Watching },
+      ];
+    },
+    songStatus: true,
   },
   spotifyClientId: "f71a3da30e254962965ca2a89d6f74b9",
   spotifyClientSecret: "199a619d22dd4e55a4a2c1a7a3d70e63",
-}
+};
+
+// Updating the bot status
+client.on('ready', async () => {
+  console.log(`${client.user.tag} is now online!`);
+
+  const statuses = await module.exports.status.rotateDefault(client);
+  const status = statuses[0]; // Since there's only one status now
+  client.user.setActivity(status.name, { type: status.type });
+});
